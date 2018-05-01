454209bc58d8da7972f297aa533a253ce53d3528
DiegoTrivino <diego.trivino@mail.escuelaing.edu.co>
2018-05-01 15:50:18 -0500

    Configuraciones Python
    Configuraciones Python

--- WorkerContest/settings.py
+++ WorkerContest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ys4_7!=cw+dd+rm5i8g14j688%04mxy1q@z^v9533a@0^iyef'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'WorkerContest.urls'
